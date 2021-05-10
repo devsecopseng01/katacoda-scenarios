bb432d564e38f879ec27b9130d2a294bded808a2
Tüzes <Gyoergy-Daniel.Tuezes@telekom.com>
2021-05-10 17:44:15 +0200

    12313

--- DockerChallange/foreground.sh
+++ DockerChallange/foreground.sh
@@ -1,3 +1,3 @@
-echo "Starting Katacoda... This may take a few moments"
+echo "Starting Katacoda... This may take a few moments [[HOST_IP]]"
 
 while [ ! -f /usr/local/bin/start.sh ]; do sleep 1; done; sleep 1; /usr/local/bin/start.sh
