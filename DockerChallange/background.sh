063e9f199c81edc2f637a85e43149ca402d2170d
Tüzes <Gyoergy-Daniel.Tuezes@telekom.com>
2021-05-10 17:43:26 +0200

    Update background.sh

--- DockerChallange/background.sh
+++ DockerChallange/background.sh
@@ -1,6 +1,12 @@
-echo "Started [[HOST_IP]]"
+echo "Started"
 
-cat << 'EOF' > /opt/test.sh
+cat << 'EOF' > /root/multiply.js
+function multiply(a, b){
+  a * b
+}
+EOF
+
+cat << 'EOF' > /opt/test.js
 var x = multiply(2, 2);
 if(x === 4) {
   process.exit(0);
