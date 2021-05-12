283d98a6c7957962dfff578845f5ac5669f9a7f1
devsecopseng01 <83073851+devsecopseng01@users.noreply.github.com>
2021-05-12 14:40:43 +0200

    Update background.sh

--- DockerChallange/background.sh
+++ DockerChallange/background.sh
@@ -1,22 +1,5 @@
 echo "Started"
 
-cat << 'EOF' > /root/script.sh
-#!/bin/bash
-
-
-VAR0="nginx:alpine"
-VAR1=`docker ps | grep $VAR0 | wc -l
-echo $VAR1
-if [ $VAR1 -ge 1 ]
-then
-        echo "Ngninx is running"
-
-else
-
-echo "nginx is not running"
-
-fi
-EOF
 
 cat << 'EOF' > /opt/test.js
 var x = multiply(2, 2);
