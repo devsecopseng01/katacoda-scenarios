72d2c90930c2ebb76e3a9b2de594cdef34af6cb9
devsecopseng01 <83073851+devsecopseng01@users.noreply.github.com>
2021-05-12 14:59:16 +0200

    refactor

--- Dchallenge/foreground.sh
+++
@@ -1,5 +0,0 @@
-echo "Starting Katacoda... This may take a few moments [[HOST_IP]]"
-
-echo "[[HOST_IP]] docker" >> /etc/hosts 
-
-while [ ! -f /usr/local/bin/start.sh ]; do sleep 1; done; sleep 1; /usr/local/bin/start.sh
