a35806c52330c457daf7967d085b33774c022593
devsecopseng01 <83073851+devsecopseng01@users.noreply.github.com>
2021-05-19 16:39:21 +0200

    add node program

--- DockerChallange/background.sh
+++ DockerChallange/background.sh
@@ -3,6 +3,8 @@ echo "Started"
 
 cp /usr/local/bin/hi.sh /root/hi.sh
 
+cp /usr/local/bin/node_program.js /root/node_program.js
+
 cat << 'EOF' > /opt/test.js
 var x = multiply(2, 2);
 if(x === 4) {
