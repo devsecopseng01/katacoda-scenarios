41360485c0f9f26cfe6d4897c6dc2817952cfae2
devsecopseng01 <83073851+devsecopseng01@users.noreply.github.com>
2021-05-19 16:34:39 +0200

    intro text + fix hi.sh

--- DockerChallange/assets/hi.sh
+++ DockerChallange/assets/hi.sh
@@ -3,15 +3,15 @@
 
 VAR0="wordpress"
 VAR1=`docker ps | grep $VAR0 | wc -l`
-VAR2=`curl docker:8080 -s | grep Empanadas | wc -l`
+VAR2=`curl docker:80 -s | grep Empanadas | wc -l`
 echo $VAR1
 echo $VAR2
 if [ $VAR1 -ge 1 ] && [ $VAR2 -ge 1 ]
 then
-        echo "Nginx is running"
+        echo "${VAR0} is running"
 
 else
 
-echo "nginx is not running"
+echo "${VAR0} is not running"
 
 fi
