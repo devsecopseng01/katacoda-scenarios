9c4ccf4242c705d8f947da5c9149535f14f015f8
devsecopseng01 <83073851+devsecopseng01@users.noreply.github.com>
2021-05-20 12:29:33 +0200

    Update challenge.sh

--- DockerChallange/assets/challenge.sh
+++ DockerChallange/assets/challenge.sh
@@ -10,7 +10,7 @@ kc_start_tasks()
   kc_task \
       "Task 1: Fix multiply.js to execute properly" \
       "Well Done! This is a common issue in JavaScript and something that catches out everyone at some point." \
-      "cat /root/multiply.js /opt/test.js > /opt/.run.js; node /opt/.run.js;"
+      "cat /opt/test.js > /opt/.run.js; node /opt/.run.js;"
 
 
   echo "You successfully fixed the application"
