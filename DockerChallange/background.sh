f41de8d4a68e664bdd5c3f827fa9fa2e9a818b55
devsecopseng01 <83073851+devsecopseng01@users.noreply.github.com>
2021-05-20 12:23:52 +0200

    Update background.sh

--- DockerChallange/background.sh
+++ DockerChallange/background.sh
@@ -6,13 +6,18 @@ cp /usr/local/bin/hi.sh /root/hi.sh
 cp /usr/local/bin/node_program.js /root/node_program.js
 
 cat << 'EOF' > /opt/test.js
-var x = multiply(2, 2);
-if(x === 4) {
-  process.exit(0);
-} else {
-  console.log("Error", x);
-  process.exit(1);
-}
+const { exec } = require('child_process');
+exec('sh hi.sh', (err, stdout, stderr) => {
+  if (err) {
+    //some err occurred
+    console.error(err)
+  } else {
+   // the *entire* stdout and stderr (buffered)
+   console.log(`out: ${stdout}`);
+    process.exit(0);
+   console.log(`stderr: ${stderr}`);
+  }
+});
 EOF
 
 
