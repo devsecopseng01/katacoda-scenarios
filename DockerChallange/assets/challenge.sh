78b065a980645cbfa2fc39cdccd653fd2001c8c2
devsecopseng01 <83073851+devsecopseng01@users.noreply.github.com>
2021-05-20 13:27:00 +0200

    Update challenge.sh

--- DockerChallange/assets/challenge.sh
+++ DockerChallange/assets/challenge.sh
@@ -4,12 +4,12 @@ kc_start_tasks()
 {
   echo "Debug Code: JavaScript 1"
 
-  echo "Complete the following tasks to progress:"
+  echo "Complete the following task to progress:"
   echo
 
   kc_task \
-      "Task 1: Fixessss multiply.js to execute properly" \
-      "Well Done! This is a common issue in JavaScript and something that catches out everyone at some point." \
+      "Task 1: Create a Wordpress container with all the neccesary steps using MariaDB. Once created make sure to call your website Empanadas " \
+      "Well Done! You have successfully set up a WordPress site using docker." \
       "cat /opt/test.js > /opt/.run.js; node /opt/.run.js;"
 
 
